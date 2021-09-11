ce089007035923eb6c8a1f377c321e7417a87e41
jerry <hiltns@163.com>
2021-09-11 23:38:39 +0800

    First commit

---
+++ js/LeafletMap.js
@@ -0,0 +1,5 @@
+var map;
+function init(){
+	map = L.map("map").setView([30.56486,114.353622], 10);
+	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}',{foo:'bar'}.addTo(map));
+}
