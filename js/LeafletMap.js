0e6c337171f468a93ccf4b8779c5a154e430fad0
jerry <hiltns@163.com>
2021-09-12 00:23:30 +0800

    代码优化：将js、css、code分开

--- js/LeafletMap.js
+++ js/LeafletMap.js
@@ -1,5 +1,7 @@
 var map;
 function init(){
 	map = L.map("map").setView([30.56486,114.353622], 10);
-	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}',{foo:'bar'}.addTo(map));
+	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png? {foo}', {foo:'bar'}).addTo(map);
+
+
 }
